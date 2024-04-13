0612c45c6352cf921394e7c62fab85431f4f027e
bikuta6 <victor.ramos.osuna@alumnos.upm.es>
2024-04-13 14:47:23 +0200

    .cql y .sql funcionales
    (solo hay que ajustar la ruta en el .sql

--- Parte1/leaderboards/queries_for_data.sql
+++ Parte1/leaderboards/queries_for_data.sql
@@ -1,21 +1,25 @@
 select webuser.country, dungeon.idD as dungeon_id, dungeon.name as dungeon_name, webuser.email, webuser.userName as user_name, completeddungeons.time as time_minutes, completeddungeons.date
 from completeddungeons 
 inner join dungeon on completeddungeons.idD = dungeon.idD
-inner join webuser on webuser.email = completeddungeons.email;
+inner join webuser on webuser.email = completeddungeons.email
+INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/dungeon_completion.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
 
-select webuser.email, COUNT(*) as n_kills
-from event 
-inner join kills on event.idE = kills.idE
-inner join webuser on webuser.email = kills.email
-WHERE webuser.country = 'de_DE' AND event.idE = 0
-GROUP BY webuser.country, event.idE, webuser.email
-ORDER BY n_kills DESC;
 
 select completeddungeons.email, completeddungeons.idD as dungeon_id, completeddungeons.time as time_minutes, completeddungeons.date
-from completeddungeons;
+from completeddungeons
+INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/player_stats.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
 
-select email, COUNT(*) from kills 
-
-WHERE country='de_DE' AND idE=0 GROUP BY country, event_id, email;
-
-select * from kills;
+select webuser.country as country, event.idE as event_id, webuser.email as email, webuser.userName as user_name, kills.idM as monster_id, kills.idK as kill_id
+from event
+inner join kills on event.idE = kills.idE
+inner join webuser on webuser.email = kills.email
+INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/kill_event.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
