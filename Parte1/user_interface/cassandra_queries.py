b3e1609e706d7e35bb1e0c0918d27a7cb397f9ca
bikuta6 <vramososuna@gmail.com>
2024-04-18 17:24:05 +0200

    fixed cassandra query
    In case country doesn't have certain dungeon, giving index error

--- Parte1/user_interface/cassandra_queries.py
+++ Parte1/user_interface/cassandra_queries.py
@@ -23,22 +23,25 @@ def leaderboard_by_country(country):
     session.set_keyspace("dungeons")
     final = []
     for i in range(20):
-        to_insert = {}
-        
-        query = f"SELECT dungeon_name, email, user_name, time_minutes, date from hall_of_fame WHERE country='{country}' AND dungeon_id={i} LIMIT 5;"
-        aux = session.execute(query)
-        dungeon_id = i
-        res = [{'dungeon_name':row.dungeon_name, 'email': row.email, 'user_name': row.user_name,
-        'time_minutes': row.time_minutes, 'date': row.date} for row in aux]
-        dungeon_name = res[0]['dungeon_name']
-        to_insert['dungeon_id'] = i
-        to_insert['dungeon_name'] = dungeon_name
-        to_insert['Top_5'] = []
-        for e in res:
-            del e['dungeon_name']
-            to_insert['Top_5'].append(e)
+        try:
+            to_insert = {}
             
-        final.append(to_insert)
+            query = f"SELECT dungeon_name, email, user_name, time_minutes, date from hall_of_fame WHERE country='{country}' AND dungeon_id={i} LIMIT 5;"
+            aux = session.execute(query)
+            dungeon_id = i
+            res = [{'dungeon_name':row.dungeon_name, 'email': row.email, 'user_name': row.user_name,
+            'time_minutes': row.time_minutes, 'date': str(row.date)} for row in aux]
+            dungeon_name = res[0]['dungeon_name']
+            to_insert['dungeon_id'] = i
+            to_insert['dungeon_name'] = dungeon_name
+            to_insert['Top_5'] = []
+            for e in res:
+                del e['dungeon_name']
+                to_insert['Top_5'].append(e)
+                
+            final.append(to_insert)
+        except:
+            pass
     return json.dumps(final, indent=4)
 
 
@@ -50,5 +53,5 @@ def user_stats_per_dun(email, dungeon_id):
     query = f"select time_minutes, date from user_stats where email='{email}' AND dungeon_id={dungeon_id};"
     aux = session.execute(query)
     for row in aux:
-        final.append({'time_minutes':row.time_minutes, 'date':row.date}) 
+        final.append({'time_minutes':row.time_minutes, 'date': str(row.date)}) 
     return json.dumps(final, indent=4)
