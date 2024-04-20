57e286d627591144710ca54bc94d2ffb2b518e5a
bikuta6 <vramososuna@gmail.com>
2024-04-20 12:36:02 +0200

    prettier query results (cassandra)

--- Parte1/user_interface/cassandra_queries.py
+++ Parte1/user_interface/cassandra_queries.py
@@ -1,5 +1,6 @@
 from cassandra.cluster import Cluster
 import json
+import pandas as pd
 
 
 def sortFn(dict):
@@ -13,8 +14,11 @@ def topk_players(country, event_id, k):
     query =f'''SELECT email, user_name, COUNT(*) as n_killed from top_horde WHERE country='{country}' AND event_id={event_id} GROUP BY country, event_id, email;'''
     raw = [{'email': row.email, 'user_name': row.user_name, 'n_killed': row.n_killed} for row in session.execute(query)]
     sorted_data = sorted(raw, key=lambda x: x['n_killed'], reverse=True)
-    top_k = json.dumps(sorted_data[:k], indent=4)
-    return top_k
+    df = pd.DataFrame(sorted_data[:k])
+    positions = [str(i)+'º' for i in range(1, k+1)]
+    df.insert(0, 'Position', positions)
+    df.set_index('Position', inplace=True)
+    return df
 
 
 def leaderboard_by_country(country):
@@ -42,7 +46,14 @@ def leaderboard_by_country(country):
             final.append(to_insert)
         except:
             pass
-    return json.dumps(final, indent=4)
+    headers = ['dungeon_id', 'dungeon_name', 'email', 'user_name', 'time_minutes', 'date']
+    array_for_df = []
+    for e in final:
+        for i in e['Top_5']:
+            array_for_df.append([e['dungeon_id'], e['dungeon_name'], i['email'], i['user_name'], i['time_minutes'], i['date']])
+    df = pd.DataFrame(array_for_df, columns=headers)
+    df.set_index(['dungeon_id', 'dungeon_name'], inplace=True)
+    return df
 
 
 def user_stats_per_dun(email, dungeon_id):
@@ -53,5 +64,6 @@ def user_stats_per_dun(email, dungeon_id):
     query = f"select time_minutes, date from user_stats where email='{email}' AND dungeon_id={dungeon_id};"
     aux = session.execute(query)
     for row in aux:
-        final.append({'time_minutes':row.time_minutes, 'date': str(row.date)}) 
-    return json.dumps(final, indent=4)
+        final.append({'time_minutes':row.time_minutes, 'date': str(row.date)})
+    df = pd.DataFrame(final) 
+    return df
